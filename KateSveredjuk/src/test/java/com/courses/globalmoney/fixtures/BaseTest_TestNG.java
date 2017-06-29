0859fbca2fda04e3354db71f85c406b1b3d0eb20
katya.svereduk <katya.svereduk@gmail.com>
2017-06-29 11:39:32 +0300

    Added TestNG_CrossBrowser.xml

--- KateSveredjuk/src/test/java/com/courses/globalmoney/fixtures/BaseTest_TestNG.java
+++ KateSveredjuk/src/test/java/com/courses/globalmoney/fixtures/BaseTest_TestNG.java
@@ -1,16 +1,18 @@
 package com.courses.globalmoney.fixtures;
 
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
 import org.openqa.selenium.TakesScreenshot;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 
 
 import javax.xml.crypto.Data;
@@ -22,13 +24,30 @@ public class BaseTest_TestNG {
 
     protected static WebDriver driver;
 
-
+    @Parameters({"browser"})
     @BeforeClass(alwaysRun = true)
-    public static void setAll() {
-        ChromeDriverManager.getInstance().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    public void openBrowser(String browser) {
+        try {
+            if (browser.equalsIgnoreCase("Chrome")) {
+                ChromeDriverManager.getInstance().setup();
+                driver = new ChromeDriver();
+
+            } else if (browser.equalsIgnoreCase("Firefox")) {
+                FirefoxDriverManager.getInstance().setup();
+                driver = new FirefoxDriver();
+            } else if (browser.equalsIgnoreCase("IE")) {
+                InternetExplorerDriverManager.getInstance().setup();
+                driver = new InternetExplorerDriver();
+            }
+        } catch (WebDriverException e) {
+            System.out.println(e.getMessage());
+        }
     }
+//    public static void setAll() {
+//        ChromeDriverManager.getInstance().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//    }
 
 
     @AfterMethod
@@ -46,7 +65,7 @@ public class BaseTest_TestNG {
 
     @AfterClass
     public static void tearDown() {
-        if (driver!=null)
+        if (driver != null)
             driver.quit();
     }
 
