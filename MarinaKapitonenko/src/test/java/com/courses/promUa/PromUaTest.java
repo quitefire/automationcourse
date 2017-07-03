9be2d38bb010ccf0ad356555cfa6ae3328e72832
Marina Kapitonenko <marisabell.krapka@gmail.com>
2017-07-04 00:33:11 +0300

    promUA test scenario with steps

--- MarinaKapitonenko/src/test/java/com/courses/promUa/PromUaTest.java
+++ MarinaKapitonenko/src/test/java/com/courses/promUa/PromUaTest.java
@@ -17,6 +17,14 @@ import static com.codeborne.selenide.Configuration.browser;
 
 public class PromUaTest {
 
+	private HomePage homePage = new HomePage();
+	private Header header = new Header();
+	private RegistrationPage registration = new RegistrationPage();
+	private Menu menu = new Menu();
+	private CategoryPage categoryPage = new CategoryPage("Тельняшка");
+	private ShoppingCart shoppingCart = new ShoppingCart();
+	private OrderPage orderPage = new OrderPage();
+
 	@BeforeClass
 	public static void setUp() {
 		ChromeDriverManager.getInstance().setup();
@@ -24,52 +32,61 @@ public class PromUaTest {
 		baseUrl = "https://prom.ua";
 	}
 
-
 	@Parameters({"username", "email", "password"})
 	@Test(enabled = true)
 	@Title("Заказ двух тельняшек")
 	public void testScenario(@Optional String username, @Optional String email, @Optional String password) {
-
-		HomePage homePage = new HomePage();
-		Header header = new Header();
-		RegistrationPage registration = new RegistrationPage();
-		Menu menu = new Menu();
-		CategoryPage categoryPage = new CategoryPage("Тельняшка");
-		ShoppingCart shoppingCart = new ShoppingCart();
-		OrderPage orderPage = new OrderPage();
 
 		Faker fake = new Faker();
 		if (username == null) username = fake.name().username();
 		if (email == null) email = fake.internet().emailAddress();
 		if (password == null) password = fake.internet().password();
 
+		openHomePage();
+		registerAsCustomer(username, email, password);
+		navigateToTelnyashki();
+		addCheap();
+		addExpensive();
+		checkOrderForm();
+	}
+
+	@Step("1. Перейти на потрал prom.ua")
+	private void openHomePage(){
 		homePage.open();
+	}
+
+	@Step("2. Пройти регистрацию как покупатель")
+	private void registerAsCustomer(String username, String email, String password){
 		header.getRegisterAsBtn(RegistrationPage.UserType.CUSTOMER).click();
 		registration.registerAs(new User(username, email, password));
 		header.checkUserIsLoggedIn();
+	}
 
+	@Step("3.1. Перейти в Военторг / Одежда и обувь / Тельняшки")
+	private void navigateToTelnyashki(){
 		header.getHomeBtn().click();
 		menu.category("Военторг")
 				.subCategory("Одежда и обувь")
 				.subCategory("Тельняшки")
 				.select();
-
-		categoryPage.bookCheapProduct();
-		shoppingCart.close();
-
-		categoryPage.resetFilters();
-		categoryPage.bookExpensiveProduct();
-
-		shoppingCart.getProducts().shouldHaveSize(2);
-		shoppingCart.createOrder();
-
-		orderPage.shouldBeDisplayed();
-
 	}
 
-	@Step("Перейти на потрал prom.ua")
-	private void openHomePage(){}
+	@Step("3.2. Добавить в корзину самую дешевую тельняшку")
+	private void addCheap(){
+		categoryPage.bookCheapProduct();
+		shoppingCart.close();
+	}
 
+	@Step("3.3. Добавить в корзину самую дорогую тельняшку")
+	private void addExpensive(){
+		categoryPage.resetFilters();
+		categoryPage.bookExpensiveProduct();
+	}
 
-
+	@Step("4. Проверить открытие форм оформления заказов")
+	private void checkOrderForm(){
+		shoppingCart.getProducts().shouldHaveSize(2);
+		shoppingCart.createOrder();
+		orderPage.shouldBeDisplayed();
+	}
 }
