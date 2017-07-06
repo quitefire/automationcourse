ca023bad29cf30dc337eb3a4c01e0738912086ea
quitefire <b.handozhynski@gmail.com>
2017-07-06 18:09:45 +0300

    add feature and story for allure

--- rest-api/src/main/java/com/demo/api/CurrentWeatherApi.java
+++ rest-api/src/main/java/com/demo/api/CurrentWeatherApi.java
@@ -1,6 +1,7 @@
 package com.demo.api;
 
 import com.google.common.collect.ImmutableMap;
+import ru.yandex.qatools.allure.annotations.Step;
 
 import java.util.Map;
 
@@ -21,21 +22,23 @@ public class CurrentWeatherApi {
                 as(type);
     }
 
+    @Step("Get weather by city name : {0}")
     public <T> T getWeatherByCity(String city, Class<T> type) {
         Map<String, Object> params = ImmutableMap.of(CITY, city);
         return getWeatherBy(type, params);
     }
-
+    @Step("Get weather by zip code : {0}")
     public <T> T getWeatherByZip(int code, Class<T> type) {
         Map<String, Object> params = ImmutableMap.of(ZIP_CODE, code);
         return getWeatherBy(type, params);
     }
-
+    @Step("Get weather by latitude:{0} and longitude:{1}")
     public <T> T getWeatherByCoordinates(int lat, int lon, Class<T> type) {
         Map<String, Object> params = ImmutableMap.of(LAT, lat, LON, lon);
         return getWeatherBy(type, params);
     }
 
+    @Step("Get weather by city:{0} and country code:{1}")
     public <T> T getWeatherByCityAndCountryCode(String city, String countryCode, Class<T> type) {
         Map<String, Object> params = ImmutableMap.of(CITY, String.join(",", city, countryCode));
         return getWeatherBy(type, params);
--- rest-api/src/test/java/com/demo/CurrentWeatherTest.java
+++ rest-api/src/test/java/com/demo/CurrentWeatherTest.java
@@ -5,14 +5,20 @@ import com.demo.fixtures.BaseTest;
 import com.demo.listeners.LogListener;
 import com.demo.model.ErrorResponse;
 import com.demo.model.WeatherResponse;
+import org.apache.http.HttpStatus;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Stories;
 
 import static com.demo.enums.City.LDN;
 import static com.demo.matchers.ErrorResponseAssert.assertThat;
 import static com.demo.matchers.WeatherResponseAssert.assertThat;
+import static org.apache.http.HttpStatus.*;
 
+@Features("Get current weather")
+@Stories("story-1")
 @Listeners(LogListener.class)
 public class CurrentWeatherTest extends BaseTest {
 
@@ -23,7 +29,7 @@ public class CurrentWeatherTest extends BaseTest {
     public void shouldGetWeatherByCity() {
         String cityName = LDN.getCity();
         WeatherResponse weatherResponse = api.getWeatherByCity(cityName, WeatherResponse.class);
-        assertThat(weatherResponse).hasName(cityName).hasCod(200);
+        assertThat(weatherResponse).hasName(cityName).hasCod(SC_OK);
         Assertions.assertThat(weatherResponse.getSys().getCountry()).isEqualTo(LDN.getCoutryCode());
     }
     // end::shouldGetWeatherByCity[]
@@ -34,7 +40,7 @@ public class CurrentWeatherTest extends BaseTest {
         String cityName = LDN.getCity();
         String code = LDN.getCoutryCode();
         WeatherResponse weatherResponse = api.getWeatherByCityAndCountryCode(cityName, code, WeatherResponse.class);
-        assertThat(weatherResponse).hasName(cityName).hasCod(200);
+        assertThat(weatherResponse).hasName(cityName).hasCod(SC_OK);
         Assertions.assertThat(weatherResponse.getSys().getCountry()).isEqualTo(code);
     }
     // end::shouldGetWeatherByCityAndCountryCode[]
@@ -44,7 +50,7 @@ public class CurrentWeatherTest extends BaseTest {
     public void shouldGetWeatherByZipCode() {
         WeatherResponse weatherResponse = api.getWeatherByZip(94040, WeatherResponse.class);
         assertThat(weatherResponse).hasName("Mountain View")
-                .hasCod(200);
+                .hasCod(SC_OK);
     }
     // end::shouldGetWeatherByZipCode[]
 
@@ -52,13 +58,13 @@ public class CurrentWeatherTest extends BaseTest {
     public void shouldGetWeatherByCoordinates() {
         WeatherResponse weatherResponse = api.getWeatherByCoordinates(35, 139, WeatherResponse.class);
         assertThat(weatherResponse).hasName("Shuzenji")
-                .hasCod(200);
+                .hasCod(SC_OK);
     }
 
     @Test
     public void shouldNotGetNonExistingCity() {
         ErrorResponse errorResponse = api.getWeatherByCity("/", ErrorResponse.class);
         assertThat(errorResponse).hasMessage("city not found")
-                .hasCod(404);
+                .hasCod(SC_NOT_FOUND);
     }
 }
